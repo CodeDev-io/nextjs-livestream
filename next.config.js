7b6182832c978dcb700df19f09521c08c6b65e7e
CodeDev-io <liveforever2022unlimited@gmail.com>
2026-01-30 20:56:48 +0200

    4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Forces Next.js to produce static HTML/CSS/JS
-  images: {
-    unoptimized: true, // Required for 'export' since Next's Image Optimization needs a server
-  },
     experimental: {
     optimizePackageImports: ["@radix-ui/react-icons"],
   },
